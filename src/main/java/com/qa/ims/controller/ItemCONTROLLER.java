873685a4bea8168f13222b2151ce0ca9b19f6271
junaidaziz68 <junaidaziz68@outlook.com>
2022-03-29 13:48:01 +0100

    ja : my first commit

---
+++ src/main/java/com/qa/ims/controller/ItemCONTROLLER.java
@@ -0,0 +1,25 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+public class ItemCONTROLLER implements CrudController{
+    @Override
+    public List readAll() {
+        return null;
+    }
+
+    @Override
+    public Object create() {
+        return null;
+    }
+
+    @Override
+    public Object update() {
+        return null;
+    }
+
+    @Override
+    public int delete() {
+        return 0;
+    }
+}
