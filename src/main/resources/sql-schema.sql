8e3781572afe58e5b50fb57738765bc0e861a62e
junaidaziz68 <junaidaziz68@outlook.com>
2022-04-03 22:07:34 +0100

    ja: completed

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,34 +1,34 @@
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
- USE `ims` ;
+USE `ims` ;
 
- CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-     `id` INT NOT NULL AUTO_INCREMENT,
-     `first_name` VARCHAR(40) DEFAULT NULL,
-     `surname` VARCHAR(40) DEFAULT NULL,
-     PRIMARY KEY (`id`)
- );
+CREATE TABLE IF NOT EXISTS `ims`.`customers` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) DEFAULT NULL,
+    `surname` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
 
- CREATE TABLE IF NOT EXISTS `ims`.`items` (
-     `id` INT NOT NULL AUTO_INCREMENT,
-     `item` VARCHAR(40) DEFAULT NULL,
-     `price` DECIMAL(5,2) DEFAULT NULL,
- 	PRIMARY KEY (`id`)
- );
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `item` VARCHAR(40) DEFAULT NULL,
+    `price` DECIMAL(5,2) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+);
 
- CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-     `id` INT NOT NULL AUTO_INCREMENT,
-     `customer_id` INT DEFAULT NULL,
-     PRIMARY KEY (`id`),
- 	FOREIGN KEY (`customer_id`) REFERENCES `ims`.`customers`(`id`) ON DELETE CASCADE
- );
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `customer_id` INT DEFAULT NULL,
+    PRIMARY KEY (`id`),
+	CONSTRAINT `FK_cid_1` FOREIGN KEY (`customer_id`) REFERENCES `ims`.`customers`(`id`) ON DELETE CASCADE
+);
 
- CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
-     `id` INT NOT NULL AUTO_INCREMENT,
-     `order_id` INT DEFAULT NULL,
-     `item_id` INT DEFAULT NULL,
-     PRIMARY KEY (`id`),
- 	FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders`(`id`) ON DELETE CASCADE,
- 	FOREIGN KEY (`item_id`) REFERENCES `ims`.`items`(`id`) ON DELETE CASCADE
- );
-
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `order_id` INT DEFAULT NULL,
+    `item_id` INT DEFAULT NULL,
+	`quantity` INT DEFAULT NULL,
+    PRIMARY KEY (`id`),
+	CONSTRAINT `FK_oid_1` FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders`(`id`) ON DELETE CASCADE,
+	CONSTRAINT `FK_iid_1` FOREIGN KEY (`item_id`) REFERENCES `ims`.`items`(`id`) ON DELETE CASCADE
+);
